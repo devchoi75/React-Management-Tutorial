0410f5b482989e06185b165f43336d8a030d58fc
devchoi75 <devchoi75@gmail.com>
2022-01-13 14:02:18 +0900

    master

---
+++ src/component/Customer.js
@@ -0,0 +1,17 @@
+//import moduleName from 'module'
+import React from 'react';
+
+class Customer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2> 최혁 </h2>
+        <p> 123456</p>
+        <p> 남자</p>
+        <p> 직장인</p>
+      </div>
+    );
+  }
+}
+
+export default Customer;
